65a2de36533766cecc2a43201132dbf8035d5d30
tcg <you@example.comkwanelemkhwanazi688@gmail.com>
2021-02-09 11:52:10 +0200

    I store my products in this file

---
+++ shop-app/src/data.js
@@ -0,0 +1,37 @@
+import React from 'react';
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'MacBook',
+        price: 1400,
+        image: 'https://cdn.pixabay.com/photo/2015/11/09/14/43/laptop-1035345_960_720.jpg',
+      },
+      {
+        id: '2',
+        name: 'Black Dress',
+        price: 2400,
+        image: 'https://cdn.pixabay.com/photo/2014/03/24/13/49/dress-294514_960_720.png',
+      },
+      {
+        id: '3',
+        name: 'W Shoes',
+        price: 1000,
+        image: 'https://cdn.pixabay.com/photo/2015/11/24/13/35/small-change-1060010_960_720.jpg',
+      },
+      {
+          id: '4',
+          name: 'Suite',
+          price: 2500,
+          image: 'https://cdn.pixabay.com/photo/2015/03/21/13/07/men-683655_960_720.jpg',
+      },
+    //   {
+    //     id:'5',
+    //     name: 'Suite',
+    //     price:'2500',
+    //     image: 'https://cdn.pixabay.com/photo/2015/03/21/13/07/men-683655_960_720.jpg',
+    // },
+    
+    ],
+  };
+  export default data;
