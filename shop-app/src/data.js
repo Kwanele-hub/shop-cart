d9c3c3aa4f61d9934d3c6c17f9bfdbcf95aeeeaf
tcg <you@example.comkwanelemkhwanazi688@gmail.com>
2021-02-10 15:29:38 +0200

    added the description on the products

--- shop-app/src/data.js
+++ shop-app/src/data.js
@@ -6,31 +6,36 @@ const data = {
         name: 'MacBook',
         price: 1400,
         image: 'https://cdn.pixabay.com/photo/2015/11/09/14/43/laptop-1035345_960_720.jpg',
+        description: 'MacBook Pro 15-inch Touch Bar',
       },
       {
         id: '2',
         name: 'Black Dress',
         price: 2400,
         image: 'https://cdn.pixabay.com/photo/2014/03/24/13/49/dress-294514_960_720.png',
+        description: 'Sweatshirt DressModelSAVE',
       },
       {
         id: '3',
         name: 'W Shoes',
         price: 1000,
         image: 'https://cdn.pixabay.com/photo/2015/11/24/13/35/small-change-1060010_960_720.jpg',
+        description: 'Ego Highbeam Court Heels',
       },
       {
           id: '4',
-          name: 'Suite',
+          name: 'Suit',
           price: 2500,
           image: 'https://cdn.pixabay.com/photo/2015/03/21/13/07/men-683655_960_720.jpg',
+          description: 'Grid check skinny suit jacket'
       },
-    //   {
-    //     id:'5',
-    //     name: 'Suite',
-    //     price:'2500',
-    //     image: 'https://cdn.pixabay.com/photo/2015/03/21/13/07/men-683655_960_720.jpg',
-    // },
+      {
+        id:'5',
+        name: 'Iphone 6',
+        price: 12500,
+        image: 'https://cdn.pixabay.com/photo/2013/07/12/18/39/smartphone-153650_960_720.png',
+        description: 'Apple iPhone 6 (32GB) - Space Grey'
+    },
     
     ],
   };
